dff2b5cda01c03b7b28c5d410d5736dfb58c42c1
Brian Pattison <brian@brianpattison.com>
2012-04-08 14:37:32 -0500

    Using Ember objects for TableViewRows is impossibly slow

--- ui/table_view.js
+++ ui/table_view.js
@@ -1,81 +1,32 @@
-var CollectionView = require('/lib/em_ti/ui/collection_view'),
-    TableViewRow   = require('/lib/em_ti/ui/table_view_row');
+var View = require('/lib/em_ti/ui/view');
 
-var TableView = CollectionView.extend({
+var TableView = View.extend({
   tiOptions: 'allowsSelection allowsSelectionDuringEditing anchorPoint animatedCenterPoint backgroundColor backgroundDisabledColor backgroundDisabledImage backgroundFocusedColor backgroundFocusedImage backgroundGradient backgroundImage backgroundLeftCap backgroundSelectedColor backgroundSelectedImage backgroundTopCap borderColor borderRadius borderWidth bottom center data editable editing filterAttribute filterCaseInsensitive focusable font-family font-size font-style font-weight footerTitle footerView headerTitle headerView height index left maxRowHeight minRowHeight moving opacity right rowHeight scrollable search searchHidden separatorColor separatorStyle showVerticalScrollIndicator size softKeyboardOnFocus style top touchEnabled transform visible width zIndex'.split(' '),
   tiEvents: 'click dblclick delete doubletap move scroll scrollEnd singletap swipe touchcancel touchend touchmove touchstart twofingertap'.split(' '),
-  itemViewClass: TableViewRow,
 
   createTiObject: function(options) {
     return Ti.UI.createTableView(options);
   },
 
-  addChildView: function(tiObject, childView) {
-    if (childView instanceof TableViewRow) {
-      childView.set('parentView', this);
-      tiObject.appendRow(childView.get('tiObject'));
-    } else {
-      tiObject.appendRow(childView);
-    }
+  createRow: function(content) {
+    // Example of how you might use createRow
+    return Ti.UI.createTableViewRow({
+      className: 'rowClass',
+      id: content.get('id'),
+      title: content.get('title')
+    });
   },
-
+  
   contentDidChange: function() {
-    var self = this, content = this.get('content'), childViews = this.get('childViews'), itemViewClass = this.get('itemViewClass'), tiObject = this.get('tiObject'), renderedRows = [];
-    childViews.splice(0, childViews.length);
-    if (content && content !== null && content.get('length')) {
-      content.forEach(function(el, idx) {
-        if (self.get('isRendered')) {
-          var renderedRow = self.createChildView(itemViewClass, {content: el}).render();
-          renderedRows.push(renderedRow.get('tiObject'));
-        } else {
-          var row = self.createChildView(itemViewClass, {content: el});
-          childViews.push(row);
-        }
-      });
-    }
-    if (this.get('isRendered')) {
-      tiObject.setData(renderedRows);
-    }
-  }.observes('content'),
-
-  arrayWillChange: function(content, startIdx, removedCount, addedCount) {
-    var childViews = this.get('childViews');
-
-    if (removedCount) {
-      if (this.get('isRendered')) {
-        var tiObject = this.get('tiObject');
-        for (var i = 0; i < removedCount; i++) {
-          tiObject.deleteRow(startIdx);
-        }
-      } else {
-        childViews.splice(startIdx, removedCount);
+    var content = this.get('content');
+    if (!Ember.none(content) && this.get('isRendered')) {
+      var tiObject = this.get('tiObject'), rows = [], rowObjects = [];
+      for (var i = 0; i < content.length; i++) {
+        rows.push(this.createRow(content[i]));
       }
+      tiObject.setData(rows);
     }
-  },
-
-  arrayDidChange: function(content, startIdx, removedCount, addedCount) {
-    var self = this, childViews = this.get('childViews'), itemViewClass = this.get('itemViewClass');
-
-    if (addedCount) {
-      content.slice(startIdx, startIdx+addedCount).forEach(function(el) {
-        if (self.get('isRendered')) {
-          var tiObject = self.get('tiObject'), renderedRow = self.createChildView(itemViewClass, {content: el}).render();
-          if (content.length === 1) {
-            tiObject.appendRow(renderedRow.get('tiObject'));
-          } else {
-            var insertAt = startIdx - 1;
-            if (insertAt >= 0) {
-              tiObject.insertRowAfter(insertAt, renderedRow.get('tiObject'));
-            } else {
-              tiObject.insertRowBefore(startIdx, renderedRow.get('tiObject'));
-            }
-          }
-        } else {
-          childViews.insertAt(startIdx, self.createChildView(itemViewClass, {content: el}));
-        }
-      });
-    }
-  }
+  }.observes('content')
 });
 
 module.exports = TableView;
